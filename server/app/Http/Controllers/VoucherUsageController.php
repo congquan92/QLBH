8bac5095d843fd0aa76dcb31d097981022fda9b3
Lê Hữu Thắng <>
2026-02-04 12:42:10 +0700

    huy 4/2 12h42

--- server/app/Http/Controllers/VoucherUsageController.php
+++ server/app/Http/Controllers/VoucherUsageController.php
@@ -10,6 +10,10 @@ class VoucherUsageController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function index()
+    {
+        //
+    }
 
     /**
      * Show the form for creating a new resource.
