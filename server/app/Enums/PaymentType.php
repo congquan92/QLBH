e12de962d0a9c0455a61097ffb37978871c46111
Lê Hữu Thắng <>
2026-03-05 14:22:14 +0700

    huy finish project 1.7

--- server/app/Enums/PaymentType.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums;
-
-enum PaymentType: string
-{
-    case COD = 'COD';
-    case BANK_TRANSFER = 'BANK_TRANSFER';
-}
--- server/app/Http/Responses/VariantAttribute/VariantAttributeResponse.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Responses\VariantAttribute;
-class VariantAttributeResponse {
-    public function __construct(
-        public int $id,
-        public string $attribute,
-        public string $value     
-    ) {}
-}
