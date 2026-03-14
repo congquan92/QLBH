e2ef442efda5c581a65ae3f244d4dfb10325178a
congquan92 <145172432+congquan92@users.noreply.github.com>
2026-03-14 11:00:33 +0700

    Refactor authentication handling: update imports and remove unused code in admin authentication

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { AdminAuthUtil } from "@/hooks/useAdminAuth";
+import { AdminAuthUtil } from "@/lib/AdminAuth-util";
 import { UserAuthUtil } from "@/lib/user-auth";
 import type { AxiosRequestConfig, InternalAxiosRequestConfig } from "axios";
 
